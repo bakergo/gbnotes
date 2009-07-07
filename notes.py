b65d64320a33b3603765705266058c574097f470
Greg Baker <gregorah@Spike.(none)>
2009-07-06 23:52:19 -0400

    Note subject added

--- notes.py
+++ notes.py
@@ -13,25 +13,25 @@ class NoteFile:
 		for open_file in self.open_files:
 			open_file.close()
 			self.open_files.remove(open_file)
-	def write_note(self, note):
+	def write_note(self, subject, note):
 		for open_file in self.open_files:
-				open_file.write('%s: %s' % (datetime.datetime.now().ctime(), note))
+				open_file.write('%s: %s -- %s' % (datetime.datetime.now().ctime(),subject,note))
 
 class NoteSqlite:
 	def __init__(self):
 		self.databases = list()
 	def open_note(self, path):
 		db = sqlite3.connect(path)
-		db.execute('CREATE TABLE IF NOT EXISTS Notes(time DATETIME, note TEXT);')
+		db.execute('CREATE TABLE IF NOT EXISTS Notes(time DATETIME, subj TEXT, note TEXT);')
 		self.databases.append(db)
 	def close_note(self):
 		for db in self.databases:
 			db.commit()
 			db.close()
 			self.databases.remove(db)
-	def write_note(self, note):
+	def write_note(self, subject, note):
 		for db in self.databases:
-			db.execute('INSERT INTO Notes(time, note) VALUES (current_timestamp, ?);', (note,))
+			db.execute('INSERT INTO Notes(time,subj, note) VALUES (current_timestamp, ?,?);', (subject,note))
 
 def isWriteDb():
 	for arg in sys.argv:
@@ -48,11 +48,19 @@ def isWriteFile():
 def showHelp():
 	for arg in sys.argv:
 		if(arg == '-h'):
-			print "Usage: notes [-h] [--nodb] [--nofile]"
+			print "Usage: note [-h] [--nodb] [--nofile] [SUBJ]"
 			print "-h: display this help and exit."
 			print "--nodb: does not use ~/.notes.db when writing a note."
 			print "--nofile: does not use ~/notes when writing a note."
 			exit()
+def parseSubj():
+	if(len(sys.argv) > 0):
+		if(sys.argv[len(sys.argv)-1] != '--nodb' and sys.argv[len(sys.argv)-1] != '--nofile'):
+			return sys.argv[len(sys.argv)-1]
+		else:
+			return ''
+	else:
+		return ''
 
 def makeNoteFiles():
 	notefiles = list()
@@ -71,10 +79,11 @@ print "Taking notes. ^D to quit."
 
 notefiles = makeNoteFiles()
 note = sys.stdin.readline()
+subj = parseSubj()
 
 while (len(note) != 0):
 	for notefile in notefiles:
-		notefile.write_note(note)
+		notefile.write_note(subj,note)
 	note = sys.stdin.readline()
 
 for notefile in notefiles:
