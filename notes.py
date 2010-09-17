baee8a25e7bbf9ee93a19df0caf4e4d88118618d
Greg Baker <bakergo@gmail.com>
2010-09-17 11:22:09 -0500

    Converted tabs to spaces, small pylint tweaks

--- notes.py
+++ notes.py
@@ -4,48 +4,48 @@
 import sys
 import optparse
 import os
-import NoteSqlite,NoteFile
+import NoteSqlite, NoteFile
 
 def main():
-	"""Run the arguments, then write the notes until the stream is
-	finished parsing out"""
-	parser = optparse.OptionParser(
-		usage='Take notes and store them in a database')
-	parser.add_option('--nodb', default=False, action='store_true', 
-		help='Does not use the database when writing a note.')
-	parser.add_option('--nofile', default=False, action='store_true', 
-		help='Does not use the notes file when writing a note.')
-	parser.add_option('-d','--database', default='~/.notes.db', 
-		type="string", help='Specify the database file used')
-	parser.add_option('-f','--file', default='~/notes.txt', 
-		type="string", help='Specify the note text file used')
-	parser.add_option('-s','--subject', default='', type="string",
-		help='Specify the subject of the note to be taken')
+    """Run the arguments, then write the notes until the stream is
+    finished parsing out"""
+    parser = optparse.OptionParser(
+        usage='Take notes and store them in a database')
+    parser.add_option('--nodb', default=False, action='store_true', 
+        help='Does not use the database when writing a note.')
+    parser.add_option('--nofile', default=False, action='store_true', 
+        help='Does not use the notes file when writing a note.')
+    parser.add_option('-d', '--database', default='~/.notes.db', 
+        type="string", help='Specify the database file used')
+    parser.add_option('-f', '--file', default='~/notes.txt', 
+        type="string", help='Specify the note text file used')
+    parser.add_option('-s', '--subject', default='', type="string",
+        help='Specify the subject of the note to be taken')
 
-	(options, arguments) = parser.parse_args()
+    (options, arguments) = parser.parse_args()
 
-	notefiles = list()
-	if(not options.nofile):
-		fnote = NoteFile.NoteFile()
-		fnote.open_note(os.path.expanduser(options.file))
-		notefiles.append(fnote)
-	if(not options.nodb):
-		dbnote = NoteSqlite.NoteSqlite()
-		dbnote.open_note(os.path.expanduser(options.database))
-		notefiles.append(dbnote)
-	if(len(options.subject) > 0):
-		print "Taking notes on subject %s. ^D to quit." % options.subject
-	else:
-		print "Taking notes. ^D to quit."
-	
-	note = sys.stdin.readline()	
-	while (len(note) != 0):
-		for notefile in notefiles:
-			notefile.write_note(options.subject, note)
-		note = sys.stdin.readline()
-		
-	for notefile in notefiles:
-		notefile.close_note()
+    notefiles = list()
+    if(not options.nofile):
+        fnote = NoteFile.NoteFile()
+        fnote.open_note(os.path.expanduser(options.file))
+        notefiles.append(fnote)
+    if(not options.nodb):
+        dbnote = NoteSqlite.NoteSqlite()
+        dbnote.open_note(os.path.expanduser(options.database))
+        notefiles.append(dbnote)
+    if(len(options.subject) > 0):
+        print "Taking notes on subject %s. ^D to quit." % options.subject
+    else:
+        print "Taking notes. ^D to quit."
+    
+    note = sys.stdin.readline() 
+    while (len(note) != 0):
+        for notefile in notefiles:
+            notefile.write_note(options.subject, note)
+        note = sys.stdin.readline()
+        
+    for notefile in notefiles:
+        notefile.close_note()
 
 if(__name__ == "__main__"):
-	sys.exit(main())
+    sys.exit(main())
